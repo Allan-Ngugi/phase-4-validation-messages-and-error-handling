504b014ac1a6618f7292343073bd366752c092ab
Allan-Ngugi <blessedallan1@gmail.com>
2022-11-27 09:56:10 +0300

    Solution

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
+  const [errors, setErrors] = useState([]);
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
@@ -12,7 +13,14 @@ function MovieForm() {
     category: "",
     discount: false,
     female_director: false,
-  });
+  })
+  .then((response) => {
+    if (response.ok) {
+      response.json().then((newMovie) => console.log(newMovie));
+    } else {
+      response.json().then((errorData) => setErrors(errorData.errors));
+    }
+  })
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -22,9 +30,13 @@ function MovieForm() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+    }).then((response) => {
+      if (response.ok) {
+        response.json().then((newMovie) => console.log(newMovie));
+      } else {
+        response.json().then((errorData) => setErrors(errorData.errors));
+      }
+    });
   }
 
   function handleChange(e) {
@@ -126,6 +138,14 @@ function MovieForm() {
           </label>
         </FormGroup>
         <SubmitButton type="submit">Add Movie</SubmitButton>
+        {errors.length > 0 && (
+    <ul style={{ color: "red" }}>
+      {errors.map((error) => (
+        <li key={error}>{error}</li>
+      ))}
+    </ul>
+  )}
+  <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
     </Wrapper>
   );
